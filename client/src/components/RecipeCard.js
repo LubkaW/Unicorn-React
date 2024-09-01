fa6ae197b8936627d3ff086a0ce98674fa8f9b5b
Lubos Kasal <kasal.lubs@gmail.com>
2024-09-02 00:59:37 +0200

    Fix detail for RecipeDetail card

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -3,18 +3,28 @@ import logo from "../food_placeholder.png"
 import styles from "../css/recipe-card.module.css"
 import Icon from "@mdi/react";
 import {mdiPencilOutline} from "@mdi/js";
+import {useNavigate} from "react-router-dom";
 
 function RecipeCard(props) {
+
+    const navigate = useNavigate();
+
+    function handleTitleClick() {
+        navigate(`/recipeDetail?id=${props.recipe.id}`);
+    }
+
     return (
         <Card className={styles.card}>
             <Card.Img className={styles.cardImage} variant="top" src={logo}/>
             <Card.Body className={styles.cardBody}>
                 <Card.Title className={styles.cardTitle}>
-                    {props.recipe.name}
+                    <span onClick={handleTitleClick} style={{cursor: 'pointer'}}>
+                        {props.recipe.name}
+                    </span>
                     <Icon
                         size={0.8}
                         path={mdiPencilOutline}
-                        style={{ color: 'orange', cursor: 'pointer' }}
+                        style={{color: 'orange', cursor: 'pointer'}}
                         onClick={() => props.handleAddRecipe(props.recipe)}
                     />
                 </Card.Title>
